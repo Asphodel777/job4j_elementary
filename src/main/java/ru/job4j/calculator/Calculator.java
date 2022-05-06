6e045f0070272566db30e59aa3a39c6c1d9a594b
Asphodel777 <pluta_ds@intelins.ru>
2022-05-06 16:15:40 +0300

    4.1. Studentinfo   [#159033 #274611]

---
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,20 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+
+    public static void main (String[] args){
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int two = 2;
+
+        int sixDivTwo = six / two;
+        System.out.println(sixDivTwo);
+
+        int fiveMinusTwo = five - two;
+        System.out.println(fiveMinusTwo);
+
+        int fourTimeTwo = six / two;
+        System.out.println(fourTimeTwo);
+    }
+}
